e0ee472fbf512cf7591aaa1577da12e3bb54109f
SayanBasani <sayanbasani1@gmail.com>
2025-09-05 18:24:42 +0530

    make for database

--- database.php
+++ database.php
@@ -1,16 +1,30 @@
 <?php
 class Database
 {
-    protected $host  = 'localhost';
-    protected $user  = 'root';
-    protected $password = 'admin';
-    protected $database = 'maasarada';
+    protected $host;
+    protected $user;
+    protected $password;
+    protected $database;
+    protected $port;
     public $dbConnect = false;
 
     public function __construct()
     {
+        $this->host     = getenv('DB_HOST');
+        $this->user     = getenv('DB_USERNAME');
+        $this->password = getenv('DB_PASSWORD');
+        $this->database = getenv('DB_NAME');
+        $this->port     = getenv('DB_PORT');
+
         if (!$this->dbConnect) {
-            $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+            $conn = new mysqli(
+                $this->host,
+                $this->user,
+                $this->password,
+                $this->database,
+                $this->port
+            );
+
             if ($conn->connect_error) {
                 die("Error failed to connect to MySQL: " . $conn->connect_error);
             } else {
