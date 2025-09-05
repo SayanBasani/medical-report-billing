3cbf0d506a2423084fb28462cb0e0d743972c3e5
SayanBasani <sayanbasani1@gmail.com>
2025-09-05 18:51:05 +0530

    try1

--- database.php
+++ database.php
@@ -10,11 +10,11 @@ class Database
 
     public function __construct()
     {
-        $this->host     = getenv('DB_HOST');
-        $this->user     = getenv('DB_USERNAME');
-        $this->password = getenv('DB_PASSWORD');
-        $this->database = getenv('DB_NAME');
-        $this->port     = getenv('DB_PORT');
+        $this->host     = getenv('DB_HOST')     ?: $_ENV['DB_HOST'];
+        $this->user     = getenv('DB_USERNAME') ?: $_ENV['DB_USERNAME'];
+        $this->password = getenv('DB_PASSWORD') ?: $_ENV['DB_PASSWORD'];
+        $this->database = getenv('DB_NAME')     ?: $_ENV['DB_NAME'];
+        $this->port     = getenv('DB_PORT')     ?: $_ENV['DB_PORT'];
 
         if (!$this->dbConnect) {
             $conn = new mysqli(
