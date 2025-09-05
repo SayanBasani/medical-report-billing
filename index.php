2e4086e1f1aca903ba1e6df6748393de2f1981f8
SayanBasani <sayanbasani1@gmail.com>
2025-09-05 21:31:18 +0530

    some changes on index.php

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
-<?php 
-
+<?php
 session_start();
-include('inc/header.php');
 $loginError = '';
 
 if (!empty($_POST['email']) && !empty($_POST['pwd'])) {
-	include 'Invoice.php';
-	$invoice = new Invoice();
-	$user = $invoice->loginUsers($_POST['email'], $_POST['pwd']); 
-	if(!empty($user)) {
-		$_SESSION['user'] = $user[0]['first_name']." ".$user[0]['last_name'];
-		$_SESSION['userid'] = $user[0]['id'];
-		$_SESSION['email'] = $user[0]['email'];		
-		$_SESSION['address'] = $user[0]['address'];
-		$_SESSION['mobile'] = $user[0]['mobile'];
-		header("Location: home.php");
-	} else {
-		$loginError = "Invalid email or password!";
-	}
+    include 'Invoice.php';
+    $invoice = new Invoice();
+    $user = $invoice->loginUsers($_POST['email'], $_POST['pwd']); 
+    if (!empty($user)) {
+        $_SESSION['user']    = $user[0]['first_name']." ".$user[0]['last_name'];
+        $_SESSION['userid']  = $user[0]['id'];
+        $_SESSION['email']   = $user[0]['email'];		
+        $_SESSION['address'] = $user[0]['address'];
+        $_SESSION['mobile']  = $user[0]['mobile'];
+
+        header("Location: home.php");
+        exit(); // always exit after header redirect
+    } else {
+        $loginError = "Invalid email or password!";
+    }
 }
+
+include('inc/header.php'); // include header only after login check
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
